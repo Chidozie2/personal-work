6f02410ae3d68b7e90b0a69214ab94674964fbe5
charles chimezie <chimeziecharles2020@gmai.com>
2023-09-11 11:08:03 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+
+let a = 23 < 1; //false
+let b = 2 != 1; //true
+let c = "Z" > "A"; //true
+let d = "Glee" > "Glow";
+console.log(d)
+let g = "bee" > "be"
+console.log(g)
+let h = "B" > 1;
+let n = "" == false
+console.log(n)
+let i = "10" < "100"
+let j = "2" > "52";
+console.log(i)
+console.log(j)
